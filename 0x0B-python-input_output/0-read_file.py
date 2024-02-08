4c0b526c094a90fdd02f9b9dd913b6da2d6a8590
shoaib <mohamedshoaib401@gmail.com>
2024-02-08 15:46:45 +0000

    first

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,7 @@
+#!/user/bin/python3
+""" read-file function """
+
+def read_file(filename=""):
+    """ openning the file """ 
+    with open(filename, encoding="utf-8") as f:
+        print(file.read(), end="")
