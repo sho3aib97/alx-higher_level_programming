090af824ea789482d76f6e28576bbdb666b07b4f
shoaib <mohamedshoaib401@gmail.com>
2024-02-10 07:07:51 +0000

    try

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,6 +2,6 @@
 """ read-file function """
 
 def read_file(filename=""):
-    """ openning the file """ 
-    with open(filename, encoding="utf-8") as f:
-        print(f.read(), end='')
+	""" openning the file """ 
+	with open(filename, encoding="utf-8") as f:
+		print(f.read(), end='')
