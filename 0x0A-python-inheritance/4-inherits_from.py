acc8ca609eadcdbab716edfb85d8369cc182e1ea
shoaib <mohamedshoaib401@gmail.com>
2024-02-14 08:03:06 +0000

    try

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -3,4 +3,4 @@
 
 def inherits_from(obj, a_class):
     ''' function check inheritance without the type '''
-    return issubclass(obj, a_class)
+    return isinstance(obj, a_class) and type(obj) != a_class
