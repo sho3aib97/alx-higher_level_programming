91ab5064e601a455d6a84946e1b60691a27d0598
shoaib <mohamedshoaib401@gmail.com>
2024-02-15 08:47:41 +0000

    try

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,14 +4,16 @@ Creating my first base class
 '''
 
 class Base:
-    ''' base class to check other classes for repetitiveness '''
+    ''' base class to check other classes for repetitiveness
+    '''
 
     __nb_objects = 0
     def __init__(self, id=None):
-        ''' instantiation of the base class '''
+        ''' instantiation of the base class
+        '''
 
-        if id != None:
+        if id is not None:
             self.id = id
         else:
-            __nb_objects += 1
-            self.id = __nb_objects
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
